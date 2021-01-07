133f3c623e77f930812b27a9d04f6d77bd08c1ca
joaogoes1 <joao_jdgf@hotmail.com>
2021-01-06 21:09:20 -0300

    Fix show character details

--- app/src/main/java/com/joaogoes/marvelwiki/data/service/CharacterApi.kt
+++ app/src/main/java/com/joaogoes/marvelwiki/data/service/CharacterApi.kt
@@ -2,14 +2,14 @@ package com.joaogoes.marvelwiki.data.service
 
 import com.joaogoes.marvelwiki.data.response.CharacterApiResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApi {
 
-    @GET("/v1/public/characters")
+    @GET("/v1/public/characters/{characterId}")
     suspend fun getCharacter(
-        @Query("characterId") characterId: Int,
-        @Query("limit") limit: Int? = 1,
+        @Path("characterId") characterId: Int,
     ): CharacterApiResponse
 
     @GET("/v1/public/characters")
