22bf1aa33b1456ee0e86b0581f04d4d0639cc687
joaogoes1 <joao_jdgf@hotmail.com>
2021-01-07 01:00:13 -0300

    Add ActionBar and grid or list feature

--- app/src/main/java/com/joaogoes/marvelwiki/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/joaogoes/marvelwiki/presentation/characters/CharactersFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.joaogoes.marvelwiki.R
 import com.joaogoes.marvelwiki.data.model.CharacterModel
@@ -20,12 +22,36 @@ class CharactersFragment : Fragment(R.layout.characters_fragment), CharactersFra
     private val binding: CharactersFragmentBinding by viewBinding(CharactersFragmentBinding::bind)
     private val adapter = CharactersAdapter(this)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.charactersList.adapter = adapter
+        setupMenu()
         setupSwipeToRefresh()
         setupTryAgainButton()
         observeViewState()
+        observeViewType()
         viewModel.loadCharacters()
+    }
+
+    private fun setupMenu() {
+        binding.charactersFragmentToolbar.inflateMenu(R.menu.characters_fragment_menu)
+        binding.charactersFragmentToolbar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.grid_mode -> {
+                    viewModel.viewState.viewType.postValue(CharactersAdapter.ViewType.GRID)
+                    true
+                }
+                R.id.list_mode -> {
+                    viewModel.viewState.viewType.postValue(CharactersAdapter.ViewType.LIST)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun setupSwipeToRefresh() {
@@ -33,6 +59,27 @@ class CharactersFragment : Fragment(R.layout.characters_fragment), CharactersFra
             binding.swipeToRefresh.isRefreshing = true
             viewModel.loadCharacters()
         }
+    }
+
+    private fun observeViewType() {
+        viewModel.viewState.viewType.observe(viewLifecycleOwner, { viewType ->
+            val gridItem = binding.charactersFragmentToolbar.menu.findItem(R.id.grid_mode)
+            val listItem = binding.charactersFragmentToolbar.menu.findItem(R.id.list_mode)
+            when (viewType) {
+                CharactersAdapter.ViewType.LIST -> {
+                    gridItem.isVisible = true
+                    listItem.isVisible = false
+                    binding.charactersList.layoutManager = LinearLayoutManager(requireContext())
+                }
+                CharactersAdapter.ViewType.GRID -> {
+                    listItem.isVisible = true
+                    gridItem.isVisible = false
+                    binding.charactersList.layoutManager = GridLayoutManager(requireContext(), 2)
+                }
+            }
+            adapter.viewType = viewType
+            adapter.notifyDataSetChanged()
+        })
     }
 
     private fun observeViewState() {
@@ -43,7 +90,10 @@ class CharactersFragment : Fragment(R.layout.characters_fragment), CharactersFra
             binding.swipeToRefresh.isRefreshing = false
             when (state) {
                 CharactersViewState.State.EMPTY_STATE -> binding.showOnly(binding.emptyState.root)
-                CharactersViewState.State.SUCCESS -> binding.showOnly(binding.swipeToRefresh)
+                CharactersViewState.State.SUCCESS -> {
+                    binding.showOnly(binding.swipeToRefresh)
+                    binding.charactersFragmentToolbar.visibility = View.VISIBLE
+                }
                 CharactersViewState.State.ERROR -> binding.showOnly(binding.errorState.root)
                 CharactersViewState.State.LOADING -> binding.showOnly(binding.loadingState.root)
                 CharactersViewState.State.NO_CONNECTION -> binding.showOnly(binding.noConnectionState.root)
